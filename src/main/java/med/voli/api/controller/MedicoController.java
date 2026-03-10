165887e8fdc4cd61beb4f495200adcce11964338
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-10 09:27:50 -0300

    Adiciona validacoes

--- src/main/java/med/voli/api/controller/MedicoController.java
+++ src/main/java/med/voli/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voli.api.controller;
 
+import jakarta.validation.Valid;
 import med.voli.api.medico.DadosCadastroMedico;
 import med.voli.api.medico.Medico;
 import med.voli.api.medico.MedicoRepository;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
     }
 }
