7eb9178d94ba5eaac760f5af1e570a265007d157
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-10 10:08:52 -0300

    Implementa o repository e finaliza controller

--- src/main/java/med/voli/api/controller/PacienteController.java
+++ src/main/java/med/voli/api/controller/PacienteController.java
@@ -1,6 +1,13 @@
 package med.voli.api.controller;
 
+import jakarta.validation.Valid;
 import med.voli.api.paciente.DadosCadastroPaciente;
+import med.voli.api.paciente.Paciente;
+import med.voli.api.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pacientes")
 public class PacienteController {
 
-    public void cadastrar(DadosCadastroPaciente dados) {
+    @Autowired
+    private PacienteRepository repository;
 
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
     }
 }
