306ffd3be7e4cc09ab37502de54ffaf51f7f61d3
MatheusDoRosario <matheusxdargws@gmail.com>
2026-03-16 11:44:32 -0300

    Adiciona todas as validacoes

--- src/main/java/med/voli/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voli/api/domain/consulta/AgendaDeConsultas.java
@@ -1,11 +1,14 @@
 package med.voli.api.domain.consulta;
 
 import med.voli.api.domain.ValidacaoException;
+import med.voli.api.domain.consulta.validacoes.ValidadorAgendamentoDeConsulta;
 import med.voli.api.domain.medico.Medico;
 import med.voli.api.domain.medico.MedicoRepository;
 import med.voli.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultas {
@@ -19,6 +22,9 @@ public class AgendaDeConsultas {
     @Autowired
     private PacienteRepository pacienteRepository;
 
+    @Autowired
+    private List<ValidadorAgendamentoDeConsulta> validadores;
+
     public void agendar(DadosAgendamentoConsulta dados) {
         if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
             throw new ValidacaoException("Id do médico informado não existe!");
@@ -26,6 +32,8 @@ public class AgendaDeConsultas {
         if (!pacienteRepository.existsById(dados.idPaciente())) {
             throw new ValidacaoException("Id do paciente informado não existe!");
         }
+
+        validadores.forEach(v -> v.validar(dados));
 
         var medico = escolherMedico(dados);
         var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
---
+++ src/main/java/med/voli/api/domain/consulta/validacoes/ValidadorPacienteSemOutraConsultaNoDia.java
@@ -0,0 +1,23 @@
+package med.voli.api.domain.consulta.validacoes;
+
+import med.voli.api.domain.ValidacaoException;
+import med.voli.api.domain.consulta.ConsultaRepository;
+import med.voli.api.domain.consulta.DadosAgendamentoConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidadorPacienteSemOutraConsultaNoDia implements ValidadorAgendamentoDeConsulta{
+
+    @Autowired
+    private ConsultaRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        var primeiroHorario = dados.data().withHour(7);
+        var ultimoHorario = dados.data().withHour(18);
+        var pacientePossuiOutraConsultaNoDia = repository.existsByPacienteIdAndDataBetween(dados.idPaciente(), primeiroHorario, ultimoHorario);
+        if (pacientePossuiOutraConsultaNoDia) {
+            throw new ValidacaoException("Paciente já possui uma consulta agendada nesse dia");
+        }
+    }
+}
